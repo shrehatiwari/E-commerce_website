2d6a988a53fd3236cf5fdba192770e152f213353
shreha <shreha2631@gmail.com>
2022-02-27 13:31:30 +0530

    1st

---
+++ shoppinglyx/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('app.urls'))
+]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
